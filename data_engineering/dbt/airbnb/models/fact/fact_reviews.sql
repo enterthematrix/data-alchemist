0d0badd8329e635412b8d96175e1f859c507a1b5
Sanju <sanjeev.it@gmail.com>
2025-07-01 15:10:46 +1200

    added late record handling for incremental load

--- data_engineering/dbt/airbnb/models/fact/fact_reviews.sql
+++ data_engineering/dbt/airbnb/models/fact/fact_reviews.sql
@@ -11,6 +11,15 @@ select
   *
 from src_reviews
 where review_text is not null
+-- logic to load incrementally to handle late records
 {% if is_incremental() %}
-    and review_date > (select max(review_date) from {{ this }})
+  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
 {% endif %}
+-- To test: dbt run --select fact_reviews  --vars '{start_date: "2024-02-15 00:00:00", end_date: "2024-03-15 23:59:59"}'
