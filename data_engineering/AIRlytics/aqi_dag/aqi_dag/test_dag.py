ad08d3eba5954b750f245a78310ab3b0ccf0c367
Sanju <sanjeev.it@gmail.com>
2025-07-12 11:52:41 +1200

    added sys.path to resolve local modules

--- data_engineering/AIRlytics/aqi_dag/aqi_dag/test_dag.py
+++ data_engineering/AIRlytics/aqi_dag/aqi_dag/test_dag.py
@@ -1,5 +1,13 @@
+import os, sys
+from dagster import job, op
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from get_raw_aqi_data import daily_air_quality_job_local
 
-if __name__ == "__main__":
+def test_daily_air_quality_job():
     result = daily_air_quality_job_local.execute_in_process()
-    print("Success:", result.success)
+    print("daily_air_quality_job_local Success:", result.success)
+
+
+if __name__ == "__main__":
+    test_daily_air_quality_job()
+
