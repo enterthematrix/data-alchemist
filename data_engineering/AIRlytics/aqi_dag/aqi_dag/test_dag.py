85a4b1f49518382e45b212a7ba66499565fde57e
Sanju <sanjeev.it@gmail.com>
2025-07-12 15:48:38 +1200

    job rename

--- data_engineering/AIRlytics/aqi_dag/aqi_dag/test_dag.py
+++ data_engineering/AIRlytics/aqi_dag/aqi_dag/test_dag.py
@@ -1,10 +1,10 @@
 import os, sys
 from dagster import job, op
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from get_raw_aqi_data import daily_air_quality_job_local
+from aqi_dag.get_raw_aqi_data import hourly_ingest_local
 
 def test_daily_air_quality_job():
-    result = daily_air_quality_job_local.execute_in_process()
+    result = hourly_ingest_local.execute_in_process()
     print("daily_air_quality_job_local Success:", result.success)
 
 
